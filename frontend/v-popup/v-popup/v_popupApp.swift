f98fee338f6411b06245ecebbcfe051ff796e134
Oliver Bevan <oliver@volani.co.uk>
2023-05-24 22:39:58 +0100

    Initial Commit: Establish dependencies

---
+++ frontend/v-popup/v-popup/v_popupApp.swift
@@ -0,0 +1,29 @@
+//
+//  v_popupApp.swift
+//  v-popup
+//
+//  Created by Oliver Bevan on 24/05/2023.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct v_popupApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
